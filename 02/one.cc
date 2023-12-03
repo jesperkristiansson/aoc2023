c47467c09524abba2e440b3929eef16dd2633394
Jesper Kristiansson <jeppe.00@live.se>
2023-12-03 10:33:54 +0100

    day 2

--- 02/one.cc
+++ 02/one.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <algorithm>
 #include <vector>
 #include <set>
@@ -9,6 +10,62 @@
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    string line;
+
+    int sum = 0;
+    int line_i = 1;
+    while (cin)
+    {
+        getline(cin, line);
+        int num_red = 12;
+        int num_green = 13;
+        int num_blue = 14;
+        int n = line.find_first_of(':');
+        stringstream ss{line.substr(n + 1)};
+        bool possible = true;
+        while (ss)
+        {
+            int num;
+            string color;
+            ss >> num;
+            ss >> color;
+            //cout << num << ' ' << color << endl;
+            if (color.rfind("red", 0) == 0)
+            {
+                if (num > num_red)
+                {
+                    possible = false;
+                }
+            }
+            else if (color.rfind("green", 0) == 0)
+            {
+                if (num > num_green)
+                {
+                    possible = false;
+                }
+            }
+            else if (color.rfind("blue", 0) == 0)
+            {
+                if (num > num_blue)
+                {
+                    possible = false;
+                }
+            }
+            else
+            {
+                possible = false;
+            }
+
+            if(color.back() != ';' && color.back() != ','){
+                break;
+            }
+        }
+        if(possible){
+            sum += line_i;
+        }
+        ++line_i;
+    }
+    cout << sum << endl;
 }
--- 02/two.cc
+++ 02/two.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
@@ -9,6 +11,53 @@
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    string line;
+
+    int sum = 0;
+    int line_i = 1;
+    while (cin)
+    {
+        getline(cin, line);
+        int num_red = 0;
+        int num_green = 0;
+        int num_blue = 0;
+        int n = line.find_first_of(':');
+        stringstream ss{line.substr(n + 1)};
+        bool should_be_counted = true;
+        while (ss)
+        {
+            int num;
+            string color;
+            ss >> num;
+            ss >> color;
+            if (color.rfind("red", 0) == 0)
+            {
+                num_red = max(num, num_red);
+            }
+            else if (color.rfind("green", 0) == 0)
+            {
+                num_green = max(num, num_green);
+            }
+            else if (color.rfind("blue", 0) == 0)
+            {
+                num_blue = max(num, num_blue);
+            }
+            else
+            {
+                should_be_counted = false;
+            }
+
+            if(color.back() != ';' && color.back() != ','){
+                break;
+            }
+        }
+        if(should_be_counted){
+            sum += num_red*num_green*num_blue;
+        }
+        
+        ++line_i;
+    }
+    cout << sum << endl;
 }
