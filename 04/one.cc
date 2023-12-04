40eeee6ea9f62813598c92d35e6429f23f552338
Jesper Kristiansson <jeppe.00@live.se>
2023-12-04 06:50:04 +0100

    day 4

--- 04/one.cc
+++ 04/one.cc
@@ -1,14 +1,51 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
 #include <utility>
+#include <cctype>
 
 using namespace std;
 
 int main(){
-    
+    string token;
+    long sum = 0;
+    long i = 1;
+
+    cin >> token;
+    while(cin){
+        cin >> token;
+        cin >> token;
+
+        set<long> winning;
+        while(token != "|"){
+            winning.insert(stol(token));
+            cin >> token;
+        }
+        cin >> token;
+
+        long wins = 0;  
+        while(token != "Card"){
+            long n = stol(token);
+            if(winning.count(n)){
+                ++wins;
+            }
+
+            if(!cin){
+                break;
+            }
+            cin >> token;
+        }
+
+        if(wins > 0){
+            sum += 1 << (wins - 1);
+        }
+        ++i;
+    }
+    cout << sum << endl;
 }
--- 04/two.cc
+++ 04/two.cc
@@ -1,14 +1,65 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
 #include <utility>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
 int main(){
+    vector<string> cards;
+
+    while(cin){
+        string line;
+        getline(cin, line);
+        cards.push_back(line);
+    }
     
+    long num_cards = cards.size() - 1;
+    long sum = 0;
+    long nums[num_cards];
+    for(long i = 0; i < num_cards; ++i){
+        nums[i] = 1;
+    }
+
+    for(long i = 0; i < num_cards; ++i){
+        // if(nums[i] == 0){
+        //     continue;
+        // }
+
+        stringstream line{cards[i]};
+        string token;
+        line >> token;
+        line >> token;
+        line >> token;
+
+        set<long> winning;
+        while(token != "|"){
+            winning.insert(stol(token));
+            line >> token;
+        }
+        line >> token;
+
+        long wins = 0;  
+        while(line){
+            long n = stol(token);
+            if(winning.count(n)){
+                ++wins;
+                nums[i + wins] += nums[i];
+            }
+
+            line >> token;
+        }
+
+        sum += nums[i];
+    }
+
+    cout << sum << endl;
 }
