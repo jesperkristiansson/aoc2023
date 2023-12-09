ff5a5b2c4daf39ab7bcfe20a062549b8e47abbd7
Jesper Kristiansson <jeppe.00@live.se>
2023-12-09 06:41:23 +0100

    day 6, late

--- 06/one.cc
+++ 06/one.cc
@@ -1,14 +1,45 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
 #include <utility>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
 int main(){
-    
+    vector<long> times, dists;
+    string token;
+    cin >> token;
+    cin >> token;
+    while(isdigit(token[0])){
+        times.emplace_back(stol(token));
+        cin >> token;
+    }
+    while(cin){
+        cin >> token;
+        dists.emplace_back(stol(token));
+    }
+
+    long product = 1;
+    for(std::vector<long int>::size_type i = 0; i < times.size(); ++i){
+        long ways_to_win = 0;
+        
+        long total_time = times[i];
+        for(long charge_time = 0; charge_time < total_time; ++charge_time){
+            long time_left = total_time - charge_time;
+            long dist = charge_time*time_left;
+            if(dist > dists[i]){
+                ++ways_to_win;
+            }
+        }
+        product *= ways_to_win;
+    }
+    cout << product << endl;
 }
--- 06/two.cc
+++ 06/two.cc
@@ -1,14 +1,48 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
 #include <utility>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
 int main(){
+    vector<string> times, dists;
+    string token;
+    cin >> token;
+    cin >> token;
+    while(isdigit(token[0])){
+        times.emplace_back(token);
+        cin >> token;
+    }
+    while(cin){
+        cin >> token;
+        dists.emplace_back(token);
+    }
+
+    string time_str, dist_str;
+    for(std::vector<long int>::size_type i = 0; i < times.size(); ++i){
+        time_str += times[i];
+        dist_str += dists[i];
+    }
     
+    long total_time = stol(time_str);
+    long dist_to_beat = stol(dist_str);
+
+    long ways_to_win = 0;
+    for(long charge_time = 0; charge_time < total_time; ++charge_time){
+        long time_left = total_time - charge_time;
+        long dist = charge_time*time_left;
+        if(dist > dist_to_beat){
+            ++ways_to_win;
+        }
+    }
+    cout << ways_to_win << endl;
 }
