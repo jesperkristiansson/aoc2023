2bd9abdf2ad96d7bc371b43507c30419aac916bb
Jesper Kristiansson <jeppe.00@live.se>
2026-01-03 22:21:56 +0100

    day 21: part 1

--- 21/one.cc
+++ 21/one.cc
@@ -1,14 +1,61 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <climits>
 #include <algorithm>
 #include <vector>
 #include <set>
 #include <map>
 #include <limits>
 #include <utility>
+#include <cctype>
+#include <cstring>
+
+#include "readInput.hpp"
+#include "Grid.hpp"
 
 using namespace std;
 
+using Map = Grid<char>;
+
+Point findStart(const Map &map){
+    return map.findFirst('S').value();
+}
+
+int findReachableTiles(Map &map, const Point &start, int numSteps){
+    int reachableTiles = 0;
+
+    bool countEvenSteps = numSteps % 2 == 0;
+
+    set<Point> currentBatch{start};
+    for(int i = 0; i <= numSteps && !currentBatch.empty(); i++){
+        set<Point> nextBatch;
+
+        bool isEvenStep = i % 2 == 0;
+        bool countStep = isEvenStep && countEvenSteps;
+        if(countStep){
+            reachableTiles += currentBatch.size();
+        }
+
+        for(Point p : currentBatch){
+            map.at(p) = '-';
+
+            for(Direction d : {NORTH, EAST, SOUTH, WEST}){
+                Point neighbor = p + d;
+                if(map.contains(neighbor) && map.at(neighbor) == '.'){
+                    nextBatch.insert(neighbor);
+                }
+            }
+        }
+        currentBatch = nextBatch;
+    }
+
+    return reachableTiles;
+}
+
 int main(){
-    
+    auto input_mat = readInput_mat<char>();
+    Map map(input_mat);
+    Point start = findStart(map);
+    cout << findReachableTiles(map, start, 64) << endl;
 }
--- 21/run.sh
+++ 21/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-CFLAGS="-Os -Wall -Wextra -g"
+CFLAGS="-std=c++2a -O3 -Wall -Wextra -g -I../lib/src/"
 
 g++ $CFLAGS one.cc -o one
 g++ $CFLAGS two.cc -o two
